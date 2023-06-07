427e1486ef453e9201e2be9caa18bc1a1d4a617b
Cagdas Karabulut <cagdas.karabulut@gmail.com>
2023-06-07 17:13:10 +0300

    simplified

--- src/main/java/com/karabulut/javapracticespringmapper/controller/CarController.java
+++ src/main/java/com/karabulut/javapracticespringmapper/controller/CarController.java
@@ -1,8 +1,8 @@
 package com.karabulut.javapracticespringmapper.controller;
 
 import com.karabulut.javapracticespringmapper.dto.CarDto;
+import com.karabulut.javapracticespringmapper.mapper.CarMapper;
 import com.karabulut.javapracticespringmapper.model.Car;
-import com.karabulut.javapracticespringmapper.service.CarService;
 import lombok.RequiredArgsConstructor;
 import lombok.var;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CarController {
 
-    private final CarService carService;
+    private final CarMapper mapper;
 
     @GetMapping("/test-Car")
     public Car testCar(){
         var newCarDto = CarDto.builder().make("Audi").model("A4").build();
-        return carService.testForCarDtoToCar(newCarDto);
+        return mapper.toCar(newCarDto);
     }
 
     @GetMapping("/test-CarDto")
     public CarDto testCarDto(){
         var newCar = Car.builder().id(1).make("Audi").model("A5").build();
-        return carService.testForCarToCarDto(newCar);
+        return mapper.toCarDto(newCar);
     }
 
 
